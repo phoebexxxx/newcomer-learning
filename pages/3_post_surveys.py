dcda60f6eb9b0dc881b7657f62f690d01c78387c
moyan zhou <zhou0972@umn.edu>
2025-07-31 10:42:29 -0500

    update the post survey content

--- pages/3_post_surveys.py
+++ pages/3_post_surveys.py
@@ -14,23 +14,26 @@ We now ask you to complete **three short surveys** about your experience.
 """)
 
 # Replace with your real Qualtrics survey URLs
-survey_1_url = "https://umn.qualtrics.com/jfe/form/SV_51oE0yq20SQ0njg"
-survey_2_url = "https://umn.qualtrics.com/jfe/form/SV_51oE0yq20SQ0njg"
+survey_1_url = "https://umn.qualtrics.com/jfe/form/SV_eqzQHev6Xa0if1s"
+# survey_2_url = "https://umn.qualtrics.com/jfe/form/SV_eqzQHev6Xa0if1s"
 
-# Layout: two columns side by side
-left_col, right_col = st.columns(2)
+st.markdown("### 📋 Survey 1: Wikipedia knowledge")
+st.components.v1.iframe(src=survey_1_url, height=500, width=800, scrolling=True)
 
-with left_col:
-    st.markdown("### 📋 Survey 1: Wikipedia knowledge")
-    st.components.v1.iframe(src=survey_1_url, height=500, width=450, scrolling=True)
+# # Layout: two columns side by side
+# left_col, right_col = st.columns(2)
 
-with right_col:
-    st.markdown("### 📋 Survey 2: Engagement & Learning")
-    st.components.v1.iframe(src=survey_2_url, height=500, width=450, scrolling=True)
+# with left_col:
+#     st.markdown("### 📋 Survey 1: Wikipedia knowledge")
+#     st.components.v1.iframe(src=survey_1_url, height=500, width=450, scrolling=True)
+
+# with right_col:
+#     st.markdown("### 📋 Survey 2: Engagement & Learning")
+#     st.components.v1.iframe(src=survey_2_url, height=500, width=450, scrolling=True)
 
 
-st.markdown("### 📋 Survey 3: Interacting with AI")
-st.components.v1.iframe(src=survey_2_url, height=500, width=450, scrolling=True)
+# st.markdown("### 📋 Survey 3: Interacting with AI")
+# st.components.v1.iframe(src=survey_2_url, height=500, width=450, scrolling=True)
 
 if st.button("Next"):
     st.session_state.post_surveys_done = True  # ✅ set the flag
